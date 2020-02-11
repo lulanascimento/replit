720a5aa3a08ac5d638535b634b9d36bf5054d398
lulanascimento <luisunifacs@gmail.com>
2020-02-11 21:42:59 +0000

    Loop Configurado

--- cadastro.py
+++ cadastro.py
@@ -2,24 +2,30 @@ from collections import Counter
 from datetime import datetime
 import json
 
-cadastro = dict()
+
+
 idcontador = Counter()
 idcontador = 1
-#datah = datetime.datetime.now()
 
+sair = str(input(" X para fechar o programa ou enter para continuar "))
+while( sair == "c" ):
 
-cadastro['matricula'] = idcontador + 3
-cadastro['nome'] = str(input("Nome do Funcionario: "))
-cadastro['nascimento'] = int(input("Ano de Nascimento: "))
-cadastro['ctps'] = int(input("Digite o numero da carteira: "))
-cadastro['salario'] = float(input("Digite o Salario: R$ "))
-cadastro['contratacao'] = int(input("Ano de Contratacao: "))
-cadastro['cidade'] = str(input("Cidade Natal: "))
-cadastro['estado'] = str(input("Estado de Nascimento: "))
-json = json.dumps(cadastro)
-f = open("Cadastrados.json","w")
-f.write(json)
-f.close()
+    cadastro = dict()
+    cadastro['nome'] = str(input("Nome do Funcionario: "))
+    cadastro['nascimento'] = int(input("Ano de Nascimento: "))
+    cadastro['ctps'] = int(input("Digite o numero da carteira: "))
+    cadastro['salario'] = float(input("Digite o Salario: R$ "))
+    cadastro['contratacao'] = int(input("Ano de Contratacao: "))
+    cadastro['cidade'] = str(input("Cidade Natal: "))
+    cadastro['estado'] = str(input("Estado de Nascimento: "))
+    json = json.dumps(cadastro)
+    f = open("Cadastrados.json","w")
+    f.write(json)
+    f.close()
+    sair = str(input(" X para fechar o programa ou enter para continuar "))
 
-print(cadastro)
+else:
+    print("O usuario digitou x ")
+    print(cadastro)
+#print(sair)
 
